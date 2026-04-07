131c786339f1655e58a49ac8ea583905eda65255
pavitrsaxena <pavitrsaxena01@gmail.com>
2026-04-07 15:53:34 +0530

    ReadMe.py

---
+++ ReadMe.py
@@ -0,0 +1,8 @@
+PavitrSaxena-202501100700203-ECE-B
+Design a Smart Payment Processing System in Python using Object-Oriented Programming
+principles. The system should support multiple payment methods such as Credit Card, UPI,
+PayPal, and Digital Wallet, all following a common abstract interface. Each payment type must
+implement its own logic to calculate the final payable amount based on fees, taxes, cashback, or
+balance constraints. The system should demonstrate abstraction, inheritance, and runtime
+polymorphism by processing payments through a common function that works with different
+payment objects.
